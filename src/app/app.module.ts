95cc1a76064370fb00ec8ca8c8ceddc29eb17898
Oksana.Mosiichuk <okmosiichuk@gmail.com>
2023-02-20 10:35:15 +0200

    small fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,11 @@ import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeService } from './recipe-book/recipe.service';
-import { ReversePipe } from './reverse.pipe';
 import { AuthComponent } from './auth/auth.component';
-import { LoadingSpinnerComponent } from './shared/loading-spinner/loadind-spinner';
 import { AuthInterceptorService } from './auth/auth.interceptor';
-import { RecipeBookModule } from './recipe-book/recipe-book.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 
@@ -31,7 +25,6 @@ import { SharedModule } from './shared/shared.module';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    RecipeBookModule,
     ShoppingListModule,
     SharedModule
   ],
--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
-import { SharedModule } from "../shared/shared.module";
 import { ShoppingListEditComponent } from "./shopping-list-edit/shopping-list-edit.component";
 import { ShoppingListComponent } from "./shopping-list.component";
 
@@ -15,7 +14,6 @@ import { ShoppingListComponent } from "./shopping-list.component";
 		CommonModule,
     FormsModule,
 		RouterModule.forChild([{path: 'shopping-list', component: ShoppingListComponent}]),
-		SharedModule
 	],
 	bootstrap: [ShoppingListComponent]
 })
